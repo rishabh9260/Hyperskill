50763331c05b482a6c521a52ef7417aba3f4736f
Rishabh Jain <rishabhjain9260@gmail.com>
2019-08-24 22:01:02 +0530

    Stage 1 main added

---
+++ Digit Recognition/Main.java
@@ -0,0 +1,78 @@
+package recognition;
+        import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int w1=2, w2=1, w3=2, w4=4, w5=-4, w6=4, w7=2, w8=-1, w9=2, b=-5;
+        int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+
+        Scanner sc = new Scanner(System.in);
+
+        String line2 = sc.nextLine();
+        String line3 = sc.nextLine();
+        String line4 = sc.nextLine();
+
+        if (line2.charAt(0) == '_') {
+            a1 = 0;
+        } else {
+            a1 = 1;
+        }
+
+        if (line2.charAt(1) == '_') {
+            a2 = 0;
+        } else {
+            a2 = 1;
+        }
+
+        if (line2.charAt(2) == '_') {
+            a3 = 0;
+        } else {
+            a3 = 1;
+        }
+
+        if (line3.charAt(0) == '_') {
+            a4 = 0;
+        } else {
+            a4 = 1;
+        }
+
+        if (line3.charAt(1) == '_') {
+            a5 = 0;
+        } else {
+            a5 = 1;
+        }
+
+        if (line3.charAt(2) == '_') {
+            a6 = 0;
+        } else {
+            a6 = 1;
+        }
+
+        if (line4.charAt(0) == '_') {
+            a7 = 0;
+        } else {
+            a7 = 1;
+        }
+
+        if (line4.charAt(1) == '_') {
+            a8 = 0;
+        } else {
+            a8 = 1;
+        }
+
+        if (line4.charAt(2) == '_') {
+            a9 = 0;
+        } else {
+            a9 = 1;
+        }
+
+        a10 = a1*w1 + a2*w2 + a3*w3 + a4*w4 + a5*w5 + a6*w6 + a7*w7 + a8*w8 + a9*w9 + b;
+
+        if(a10 > 0) {
+            System.out.println("This number is 0");
+        } else {
+            System.out.println("This number is 1");
+        }
+    }
+}
